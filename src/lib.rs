e72a8486ddb4b3d7a53b6655ada858347076f9f8
Jason Lee <huacnlee@gmail.com>
2020-05-27 13:07:23 +0800

    version 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,17 @@ fn main() {
 extern crate lazy_static;
 
 #[macro_use]
-extern crate regex_cjk;
+macro_rules! regexp {
+    ($arg:tt) => {{
+        let rule_str = String::from($arg).replace(
+            r"\p{CJK}",
+            r"\p{Han}|\p{Hangul}|\p{Hanunoo}|\p{Katakana}|\p{Hiragana}|\p{Bopomofo}",
+        );
+        // println!("{}", rule_str);
+        let res = regex::Regex::new(&rule_str).unwrap();
+        res
+    }};
+}
 
 mod strategery;
 use crate::strategery::Strategery;
