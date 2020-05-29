645074b20fd1311ebfb1817c29df01adee6ee255
Jason Lee <huacnlee@gmail.com>
2020-05-29 16:03:07 +0800

    Avoid create regexp in call for improve performance

--- src/lib.rs
+++ src/lib.rs
@@ -62,6 +62,7 @@ macro_rules! regexp {
 mod strategery;
 use crate::strategery::Strategery;
 use regex::Regex;
+use std::ffi::{CStr, CString};
 
 lazy_static! {
     static ref FULL_DATE_RE: Regex = regexp!(r"[\s]{0,}\d+[\s]{0,}年[\s]{0,}\d+[\s]{0,}月[\s]{0,}\d+[\s]{0,}[日号][\s]{0,}");
@@ -114,7 +115,7 @@ pub fn format(text: &str) -> String {
     out = remove_full_date_spacing(&out);
     out = space_dash_with_hans(&out);
 
-    return out;
+    out
 }
 
 // removeFullDateSpacing
@@ -162,6 +163,22 @@ mod tests {
             assert_eq!(exptected, actual);
         }
     }
+
+    // use test::Bencher;
+    // #[bench]
+    // fn bench_format_50(b: &mut Bencher) {
+    //     b.iter(|| format("【野村：重申吉利汽车(00175)“买入”评级 上调目标价至17.9港元】智通财经APP获悉，野村发布报告称"));
+    // }
+
+    // #[bench]
+    // fn bench_format_100(b: &mut Bencher) {
+    //     b.iter(|| format("【野村：重申吉利汽车(00175)“买入”评级 上调目标价至17.9港元】智通财经APP获悉，野村发布报告称，【野村：重申吉利汽车(00175)“买入”评级 上调目标价至17.9港元】智通财经APP获悉，野村发布报告称"));
+    // }
+
+    // #[bench]
+    // fn bench_format_400(b: &mut Bencher) {
+    //     b.iter(|| format("【野村：重申吉利汽车(00175)“买入”评级 上调目标价至17.9港元】智通财经APP获悉，野村发布报告称，上调吉利汽车(00175)目标价12.58%，由15.9港元升至17.9港元，并维持吉汽为行业首选股，重申对其“买入”评级，坚信吉汽长远可成为行业赢家。 该行称，随着公司销量持续复苏及产品组合改善，预计今年销量可达148万辆，同比升9%，较公司原定目标销量141万辆为高。 该行又称称，上调公司今明两年每股盈利预测各13%及升毛利率0.1个百分点，以反映销量较预期高2%及产品组合改善，主要是由领克品牌带动。公司自去年8月开始已持续投资领克品牌及进行市场推广，带动领克销量环比有所改变，预期今明两年领克将占整体销量的11%及14%。 该行表示，由于低端国产车品牌在欠缺新车款及科技下，行业整合度将提升。另外，公司从去年第二季到12月为止，一直都积极推动经销商去库存，这将有利公司今年利润率复苏。"));
+    // }
 
     #[test]
     fn it_format() {
--- src/strategery.rs
+++ src/strategery.rs
@@ -1,17 +1,27 @@
 pub struct Strategery {
-    pub one: &'static str,
-    pub other: &'static str,
-    pub space: bool,
-    pub reverse: bool,
+    space: bool,
+    reverse: bool,
+    add_space_re: regex::Regex,
+    add_space_reverse_re: regex::Regex,
+    remove_space_re: regex::Regex,
+    remove_space_reverse_re: regex::Regex,
 }
 
 impl Strategery {
     pub fn new(one: &'static str, other: &'static str, space: bool, reverse: bool) -> Self {
+        let add_space_str = format!("{}{}{}{}{}", "(", one, ")(", other, ")");
+        let add_space_reverse_str = format!("{}{}{}{}{}", "(", other, ")(", one, ")");
+
+        let remove_space_str = format!("{}{}{}{}{}", "(", one, r")\s+(", other, ")");
+        let remove_space_reverse_str = format!("{}{}{}{}{}", "(", other, r")\s+(", one, ")");
+
         return Strategery {
-            one: one,
-            other: other,
             space: space,
             reverse: reverse,
+            add_space_re: regexp!(add_space_str),
+            add_space_reverse_re: regexp!(add_space_reverse_str),
+            remove_space_re: regexp!(remove_space_str),
+            remove_space_reverse_re: regexp!(remove_space_reverse_str),
         };
     }
 
@@ -26,14 +36,10 @@ impl Strategery {
     fn add_space(&self, text: &str) -> String {
         let mut out = String::from(text);
 
-        let re_str = format!("{}{}{}{}{}", "(", self.one, ")(", self.other, ")");
-        let re = regexp!(re_str);
-        out = (&re.replace_all(&out, "$1 $2")).to_string();
+        out = (&self.add_space_re.replace_all(&out, "$1 $2")).to_string();
 
         if self.reverse {
-            let re_str = format!("{}{}{}{}{}", "(", self.other, ")(", self.one, ")");
-            let re = regexp!(re_str);
-            out = (&re.replace_all(&out, "$1 $2")).to_string();
+            out = (&self.add_space_reverse_re.replace_all(&out, "$1 $2")).to_string();
         }
 
         return out;
@@ -41,15 +47,11 @@ impl Strategery {
 
     fn remove_space(&self, text: &str) -> String {
         let mut out = String::from(text);
-        let re_str = format!("{}{}{}{}{}", "(", self.one, r")\s+(", self.other, ")");
-        let re = regexp!(re_str);
 
-        out = (&re.replace_all(&out, "$1 $2")).to_string();
+        out = (&self.remove_space_re.replace_all(&out, "$1 $2")).to_string();
 
         if self.reverse {
-            let re_str = format!("{}{}{}{}{}", "(", self.other, r")\s+(", self.one, ")");
-            let re = regexp!(re_str);
-            out = (&re.replace_all(&out, "$1 $2")).to_string();
+            out = (&self.remove_space_reverse_re.replace_all(&out, "$1 $2")).to_string();
         }
 
         return out;
