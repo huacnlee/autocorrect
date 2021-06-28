67ebf69006ca4b2f3b5db088324b4782f8644182
Jason Lee <huacnlee@gmail.com>
2021-06-28 12:01:56 +0800

    Support format with string argument for cli

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use autocorrect::{format, format_html};
-use clap::{App, Arg, SubCommand};
+use clap::{crate_version, App, Arg, SubCommand};
 use glob::glob;
 use std::fs;
+use std::path::Path;
 
 pub fn main() {
   let matches = App::new("AutoCorrect")
     .author("Jason Lee <huacnlee@gmail.com")
-    .version("0.4.3")
+    .version(crate_version!())
     .about("Automatically add whitespace between CJK (Chinese, Japanese, Korean) and half-width characters (alphabetical letters, numerical digits and symbols).")
     .subcommand(
       SubCommand::with_name("format")
@@ -26,17 +27,24 @@ pub fn main() {
   if let Some(matches) = matches.subcommand_matches("format") {
     let file_name = matches.value_of("file").unwrap();
 
-    for f in glob(file_name).unwrap() {
-      let path: String;
-
-      match f {
-        Ok(_path) => path = String::from(_path.to_str().unwrap()),
-        Err(_e) => break,
+    let path_exist = Path::new(file_name).exists();
+    if path_exist {
+      for f in glob(file_name).unwrap() {
+        let path: String;
+        match f {
+          Ok(_path) => path = String::from(_path.to_str().unwrap()),
+          Err(_e) => break,
+        }
+        let raw = fs::read_to_string(path).unwrap();
+        let raw = raw.as_str();
+        if matches.is_present("html") {
+          println!("{}", format(raw));
+        } else {
+          println!("{}", format_html(raw));
+        }
       }
-
-      let raw = fs::read_to_string(path).unwrap();
-      let raw = raw.as_str();
-
+    } else {
+      let raw = file_name;
       if matches.is_present("html") {
         println!("{}", format(raw));
       } else {
