59775fc7837cb4af526e02a8b99c1c9d6a3e360c
Jason Lee <huacnlee@gmail.com>
2021-07-16 09:31:06 +0800

    Improve lint for result as each lines

--- src/code.rs
+++ src/code.rs
@@ -8,70 +8,96 @@ use std::result::Result;
 use serde_json::json;
 
 pub fn format_pairs<R: RuleType>(
-  text: &str,
-  pairs: Result<Pairs<R>, Error<R>>,
-  lint: bool,
+    text: &str,
+    pairs: Result<Pairs<R>, Error<R>>,
+    lint: bool,
 ) -> String {
-  match pairs {
-    Ok(items) => {
-      let mut out = String::new();
-      for item in items {
-        format_pair(&mut out, item, lint);
-      }
-      return out;
+    match pairs {
+        Ok(items) => {
+            let mut out = String::new();
+            for item in items {
+                format_pair(&mut out, item, lint);
+            }
+            return out;
+        }
+        Err(_err) => {
+            // return raw or empty(lint) when err
+            println!("error: {}", _err);
+            if lint {
+                return String::from("");
+            } else {
+                return String::from(text);
+            }
+        }
     }
-    Err(_err) => {
-      // return raw or empty(lint) when err
-      println!("error: {}", _err);
-      if lint {
-        return String::from("");
-      } else {
-        return String::from(text);
-      }
-    }
-  }
 }
 
 fn format_pair<R: RuleType>(text: &mut String, item: Pair<R>, lint: bool) {
-  let rule = item.as_rule();
-  let rule_name = format!("{:?}", rule);
+    let rule = item.as_rule();
+    let rule_name = format!("{:?}", rule);
 
-  // println!("rule: {}", rule_name);
+    // println!("rule: {}", rule_name);
 
-  match rule_name.as_str() {
-    "string" | "link_string" | "text" | "comment" => format_or_lint(text, item, lint),
-    _ => {
-      let mut child_count = 0;
-      let item_str = item.as_str();
-      for child in item.into_inner() {
-        format_pair(text, child, lint);
-        child_count += 1;
-      }
+    match rule_name.as_str() {
+        "string" | "link_string" | "text" | "comment" => format_or_lint(text, item, lint),
+        _ => {
+            let mut child_count = 0;
+            let item_str = item.as_str();
+            for child in item.into_inner() {
+                format_pair(text, child, lint);
+                child_count += 1;
+            }
 
-      if child_count == 0 {
-        if !lint {
-          text.push_str(item_str);
+            if child_count == 0 {
+                if !lint {
+                    text.push_str(item_str);
+                }
+            }
         }
-      }
-    }
-  };
+    };
 }
 
 fn format_or_lint<R: RuleType>(text: &mut String, item: Pair<R>, lint: bool) {
-  let (line, col) = item.as_span().start_pos().line_col();
-  let part = item.as_str();
+    let (part_line, part_col) = item.as_span().start_pos().line_col();
+    let part = item.as_str();
 
-  if lint {
-    let new_part = format(part);
-    if new_part == part {
-      return;
+    if lint {
+        let lines = part.split("\n");
+
+        // sub line in a part
+        let mut sub_line = 0;
+        for line in lines {
+            // trim start whitespace
+            let mut trimmed = line.trim_start();
+            // number of start whitespace in this line
+            let leading_spaces = line.len() - trimmed.len();
+            // trim end whitespace
+            trimmed = trimmed.trim_end();
+
+            // format trimmed string
+            let new_line = format(trimmed);
+            // println!("{}||{},{}", new_line, trimmed, new_line.eq(trimmed));
+            if new_line.eq(trimmed) {
+                continue;
+            }
+
+            let current_line = part_line + sub_line;
+            let current_col = if sub_line > 0 {
+                // col will equal numner of removed leading whitespace
+                part_col - leading_spaces
+            } else {
+                leading_spaces
+            };
+
+            let message =
+                json!({"l": current_line,"c": current_col, "old": trimmed, "new": new_line });
+
+            text.push_str(message.to_string().as_str());
+            text.push_str("\n");
+
+            sub_line += 1;
+        }
+    } else {
+        text.push_str(format(part).as_str());
     }
-
-    let message = json!({"l": line,"c": col, "old": part, "new": new_part });
-
-    text.push_str(message.to_string().as_str());
-    text.push_str("\n")
-  } else {
-    text.push_str(format(part).as_str());
-  }
 }
--- src/javascript.rs
+++ src/javascript.rs
@@ -9,34 +9,65 @@ struct JavaScriptParser;
 
 #[allow(dead_code)]
 pub fn format_javascript(text: &str, lint: bool) -> String {
-  let pairs = JavaScriptParser::parse(Rule::item, text);
-  return code::format_pairs(text, pairs, lint);
+    let pairs = JavaScriptParser::parse(Rule::item, text);
+    return code::format_pairs(text, pairs, lint);
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
+    use pretty_assertions::assert_eq;
 
-  #[test]
-  fn it_format_javascript() {
-    let example = r###"
+    #[test]
+    fn it_format_javascript() {
+        let example = r###"
 // 第 1 行注释
 // 第 2 行注释
 function helloWorld(a) {
   const a = '第 1 个';
   const b = "第 2 个" + "第 3 个";
+  const c = `这是string第1行
+  这是string第2行`;
 }
 "###;
 
-    let expect = r###"
+        let expect = r###"
 // 第 1 行注释
 // 第 2 行注释
 function helloWorld(a) {
   const a = '第 1 个';
   const b = "第 2 个" + "第 3 个";
+  const c = `这是 string 第 1 行
+  这是 string 第 2 行`;
 }
 "###;
 
-    assert_eq!(expect, format_javascript(example, false));
-  }
+        assert_eq!(expect, format_javascript(example, false));
+    }
+
+    #[test]
+    fn it_lint_javascript() {
+        let example = r###"
+    /**
+    * Hello你好
+    * 这是第2行
+    */
+    function application() {
+      let example = "这是single line单行注释";
+      console.log(`这是string第1行
+      这是string第2行
+      `)
+    }
+    "###;
+
+        let expect = r###"
+{"c":4,"l":2,"new":"* Hello 你好","old":"* Hello你好"}
+{"c":1,"l":3,"new":"* 这是第 2 行","old":"* 这是第2行"}
+{"c":0,"l":7,"new":"\"这是 single line 单行注释\"","old":"\"这是single line单行注释\""}
+{"c":0,"l":8,"new":"`这是 string 第 1 行","old":"`这是string第1行"}
+{"c":13,"l":9,"new":"这是 string 第 2 行","old":"这是string第2行"}
+    "###;
+
+        assert_eq!(expect.trim(), format_javascript(example, true).trim());
+    }
 }
