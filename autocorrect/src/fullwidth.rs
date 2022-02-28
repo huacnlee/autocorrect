e2c56d791f36511fa0edfe23871f4bcd7f1d08ea
Jason Lee <huacnlee@gmail.com>
2022-02-28 10:31:57 +0800

    Fix #19 for remove ~ fullwidth convert, this is a keyword in Markdown syntax.

--- autocorrect/src/fullwidth.rs
+++ autocorrect/src/fullwidth.rs
@@ -3,7 +3,7 @@ use regex::Regex;
 use std::collections::HashMap;
 
 const SPCIAL_PUNCTUATIONS: &str = "[.:]([ ]*)";
-const NORMAL_PUNCTUATIONS: &str = "[,!?~]([ ]*)";
+const NORMAL_PUNCTUATIONS: &str = "[,!?]([ ]*)";
 
 lazy_static! {
     static ref FULLWIDTH_MAPS: HashMap<&'static str, &'static str> = map!(
@@ -12,8 +12,7 @@ lazy_static! {
       ";" => "；",
       ":" => "：",
       "!" => "！",
-      "?" => "？",
-      "~" => "～"
+      "?" => "？"
     );
     static ref PUNCTUATION_WITH_LEFT_CJK_RE: Regex = regexp!(
         "{}{}{}",
@@ -90,7 +89,7 @@ mod tests {
           "刚刚买了一部 iPhone,好开心!" => "刚刚买了一部 iPhone，好开心！",
           "蚂蚁集团上市后有多大的上涨空间?" =>  "蚂蚁集团上市后有多大的上涨空间？",
           "我们需要一位熟悉 JavaScript、HTML5,至少理解一种框架 (如 Backbone.js、AngularJS、React 等) 的前端开发者." => "我们需要一位熟悉 JavaScript、HTML5，至少理解一种框架 (如 Backbone.js、AngularJS、React 等) 的前端开发者。",
-          "蚂蚁疾奔:蚂蚁集团两地上市~全速推进!" =>                                                        "蚂蚁疾奔：蚂蚁集团两地上市～全速推进！",
+          "蚂蚁疾奔:蚂蚁集团两地上市~全速推进!" =>                                                        "蚂蚁疾奔：蚂蚁集团两地上市~全速推进！",
           "蚂蚁集团是阿里巴巴 (BABA.N) 旗下金融科技子公司" =>                                                "蚂蚁集团是阿里巴巴 (BABA.N) 旗下金融科技子公司",
           "Dollar 的演示 $阿里巴巴.US$ 股票标签" =>                                                   "Dollar 的演示 $阿里巴巴.US$ 股票标签",
           "确保&quot;&gt;HTML Entity&lt;&quot;的字符&#34;不会被处理&#34; Ruby&amp;Go" => "确保&quot;&gt;HTML Entity&lt;&quot;的字符&#34;不会被处理&#34; Ruby&amp;Go"
