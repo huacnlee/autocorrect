89169abddc5beab391c49f9ccf1e87f32ec48a11
Jason Lee <huacnlee@gmail.com>
2022-05-31 14:38:26 +0800

    Improve map! for support last item with ,

--- autocorrect/src/lib.rs
+++ autocorrect/src/lib.rs
@@ -68,7 +68,7 @@ macro_rules! regexp {
 
 #[macro_export]
 macro_rules! map {
-    {$($key:expr => $value:expr),+} => {{
+    {$($key:expr => $value:expr),+ $(,)?} => {{
         let mut m = HashMap::new();
         $(
             m.insert($key, $value);
