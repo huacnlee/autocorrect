2ad6d1149862390911680683d39fdd5940f41cf1
Jason Lee <huacnlee@gmail.com>
2022-10-29 00:39:55 +0800

    Improve Java example

--- autocorrect-java/example/src/main/java/org/example/Main.java
+++ autocorrect-java/example/src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
+// autocorrect-disable
 package org.example;
 
 import io.github.huacnlee.AutoCorrect;
 
 public class Main {
     public static void main(String[] args) {
-        String output = AutoCorrect.format("Hello 你好");
+        String output = AutoCorrect.format("Hello你好.");
         System.out.println(output);
+
+        var configStr = "{}";
+        AutoCorrect.loadConfig(configStr);
     }
 }
